3691a6e4ca3d74adf3f9bd38e709b1c8ecb1afcd
yazbikod <aslan_murathan@hotmail.com>
2022-10-25 01:33:12 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+/******************************************************************************
+                       KALAN HAK KODLARI
+*******************************************************************************/
+
+
+class HelloWorld
+{
+    static void Main()
+    {
+        int sayi, hak = 10, toplam = 0;
+
+        do
+        {
+            Console.Write("Sayı giriniz : ");
+            sayi = int.Parse(Console.ReadLine());
+
+            if (sayi % 2 == 0)
+            {
+                toplam += sayi;
+            }
+            else
+            {
+                hak--;
+                Console.WriteLine("Kalan Hak : {0}", (hak));
+            }
+        }
+        while (hak > 0);
+        Console.WriteLine("Toplam : {0}", (toplam));
+    }
+}
